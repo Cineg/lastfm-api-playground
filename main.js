ef48a1b8eb342b8d01c5f8a318f7682c74e4fb28
cineg <cineg7@gmail.com>
2018-07-15 11:57:21 +0200

    grid rendering fixes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const APIKEY = 'dd40e7897a7d237d17d21f033ffbea3f';
 
 //get searchform from dom
 const search = document.getElementById('searchForm');
+const output = document.getElementById('output');
 
 
 search.addEventListener('submit', e =>{
@@ -13,19 +14,23 @@ search.addEventListener('submit', e =>{
     const time = document.querySelector('input[name="time"]:checked').value;
     //number of results
     const number = document.querySelector('input[name="number"]:checked').value;
+    const numbers = number * number;
+    
+
    
 
     if(username.trim() !== ''){
         fillInfo(username);
+        clearOutput();
         if(dataType == 'gettopartists'){
             getArtists(username, dataType, time)
-            .then(results => fillgridHTML(results, number))
+            .then(results => fillgridHTML(results, number, numbers))
         } else if(dataType == 'gettopalbums'){
             getAlbums(username, dataType, time)
-            .then(results => fillgridHTMLSongs(results, number));
+            .then(results => fillgridHTMLSongs(results, number, numbers));
         } else if(dataType == 'gettoptracks'){
             getTracks(username, dataType, time)
-            .then(results => fillgridHTMLSongs(results, number));
+            .then(results => fillgridHTMLSongs(results, number, numbers));
         }
     }
     else{
@@ -115,14 +120,13 @@ function fillHTML(results){
 
 }
 
-function fillgridHTML(results, number){
+function fillgridHTML(results, number, numbers){
     const max = results[0].playcount;
-    output.classList.add('grid');
+    output.classList.add(`grid${number}`);
 
     output.innerHTML = '';
 
-    for(let i=0; i<number; i++){
-        console.log(results[0]);
+    for(let i=0; i<numbers; i++){
         output.innerHTML += `
         <div class="square-tile" style="background: url('${results[i].image[3]['#text']}');">
          <span class="new badge red author-badge" data-badge-caption="">${results[i].name}</span>
@@ -131,20 +135,43 @@ function fillgridHTML(results, number){
     }
 }
 
-
-function fillgridHTMLSongs(results, number){
+//render nice grid 
+function fillgridHTMLSongs(results, number, numbers){
     const max = results[0].playcount;
-    output.classList.add('grid');
+    output.classList.add(`grid${number}`);
 
     output.innerHTML = '';
 
-    for(let i=0; i<number; i++){
-        console.log(results[0]);
-        output.innerHTML += `
-        <div class="square-tile" style="background: url('${results[i].image[3]['#text']}');">
-         <span class="title title-custom new badge blue" data-badge-caption="" >${results[i].name}</span>
-         <span class="new badge red author-badge" data-badge-caption="">${results[i].artist.name}</span>
-         <span class="new badge" data-badge-caption="scrobbles">${results[i].playcount}</span>
-         `
+    if (number == '5'){
+        for(let i=0; i<numbers; i++){
+            output.innerHTML += `
+            <div class="square-tile" style="background: url('${results[i].image[2]['#text']}');">
+             <span class="title title-custom new badge blue" data-badge-caption="" >${results[i].name}</span>
+             <span class="new badge red author-badge" data-badge-caption="">${results[i].artist.name}</span>
+             <span class="new badge" data-badge-caption="scrobbles">${results[i].playcount}</span>
+             `
+        }
+    } else {
+        for(let i=0; i<numbers; i++){
+            output.innerHTML += `
+            <div class="square-tile" style="background: url('${results[i].image[3]['#text']}');">
+             <span class="title title-custom new badge blue" data-badge-caption="" >${results[i].name}</span>
+             <span class="new badge red author-badge" data-badge-caption="">${results[i].artist.name}</span>
+             <span class="new badge" data-badge-caption="scrobbles">${results[i].playcount}</span>
+             `
+        }
+    }
+
+}
+
+//clear output
+function clearOutput(){
+
+    if(output.classList.contains('grid3')){
+        output.classList.remove('grid3');
+    } else if(output.classList.contains('grid4')){
+        output.classList.remove('grid4');
+    } else if(output.classList.contains('grid5')){
+        output.classList.remove('grid5');
     }
 }
